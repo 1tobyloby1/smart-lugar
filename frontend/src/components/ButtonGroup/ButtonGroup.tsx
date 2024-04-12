6372bad6aff22501f689ed21b2be8c636b069038
599007 <599007@stud.hvl.no>
2024-04-12 10:09:21 +0200

    Juh

--- frontend/src/components/ButtonGroup/ButtonGroup.tsx
+++ frontend/src/components/ButtonGroup/ButtonGroup.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import './ButtonGroup.css';
+import { useState } from "react";
+import "./ButtonGroup.css";
 
 const ButtonGroup = () => {
   const [selectedOption, setSelectedOption] = useState(1);
@@ -11,30 +11,45 @@ const ButtonGroup = () => {
   const calculateLeftPosition = () => {
     switch (selectedOption) {
       case 1:
-        return '0%';
+        return "0%";
       case 2:
-        return '34.995%';
+        return "34.995%";
       case 3:
-        return '69.99%';
+        return "69.99%";
       default:
-        return '0%';
+        return "0%";
     }
   };
-
+ 
   return (
     <div className="button-group">
       <button
         className="option-button"
         style={{ left: calculateLeftPosition() }}
       >
-        Option
+        
       </button>
       <div className="option-selector">
-        <button className="option-selector-button" onClick={() => handleOptionClick(1)}>Comfort</button>
-        <div className='divider' ></div>
-        <button className='option-selector-button' onClick={() => handleOptionClick(2)}>ECO</button>
-        <div className='divider'></div>
-        <button className="option-selector-button" onClick={() => handleOptionClick(3)}>ECO+</button>
+        <button
+          className="option-selector-button"
+          onClick={() => handleOptionClick(1)}
+        >
+          Comfort
+        </button>
+        <div className="divider"></div>
+        <button
+          className="option-selector-button"
+          onClick={() => handleOptionClick(2)}
+        >
+          ECO
+        </button>
+        <div className="divider"></div>
+        <button
+          className="option-selector-button"
+          onClick={() => handleOptionClick(3)}
+        >
+          ECO+
+        </button>
       </div>
     </div>
   );
