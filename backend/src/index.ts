bb1ef4e8e34e25c042e242dbff060e327b8384e8
Tobytech-dev <tobytech.dev@gmail.com>
2024-04-18 11:57:51 +0200

    Fixed deploy workflow

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,26 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from '../swagger';
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "../swagger";
 import cabinRoutes from "./routes/cabinRoutes";
 import mapRoutes from "./routes/mapRoutes";
-import cors from 'cors';
+import cors from "cors";
 
-dotenv.config();
+if (process.env.NODE_ENV === "production") {
+  dotenv.config({ path: ".env.production" });
+} else {
+  dotenv.config();
+}
 
 const app: Express = express();
 const port = process.env.PORT || 80;
 
-app.use(cors())
+app.use(cors());
 
-app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.use('/mapping', mapRoutes);
-app.use('/cabin', cabinRoutes);
+app.use("/mapping", mapRoutes);
+app.use("/cabin", cabinRoutes);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
